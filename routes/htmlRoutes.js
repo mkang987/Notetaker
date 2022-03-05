e1b7c5648e8eb449ff63c4bfcce3d556f839b706
mkang987 <mkang987@gmail.com>
2022-03-05 18:26:25 -0500

    edited htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 
 
+//Sets the route for notes.html for "GET"
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
 //Sets any other routes to direct to index.html
 router.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
-//Sets the route for notes.html for "GET"
-router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
 
 module.exports = router;
